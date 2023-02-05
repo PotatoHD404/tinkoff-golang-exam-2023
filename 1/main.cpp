63b02800919f10bf2053ba0cd73f2b4e98e63cf3
PotatoHD404 <31506993+PotatoHD404@users.noreply.github.com>
2023-02-05 13:31:05 +0300

    Partial solutions

--- 1/main.cpp
+++ 1/main.cpp
@@ -5,6 +5,21 @@ using namespace std;
 
 
 int main() {
-
+    int n, m;
+    cin >> n >> m;
+    int i, j, k;
+    k = m;
+    while(n) {
+        if(k == m) {
+            i = 0;
+            j = m % 2 == 1 ? 1 : -1;
+        }
+        k = m / 2 + (m % 2) + i * j;
+        if (m % 2 == 1 && j == 1 || m % 2 == 0 && j == -1)
+            ++i;
+        j *= -1;
+        n--;
+        cout << k << endl;
+    }
     return 0;
 }
--- 2/main.cpp
+++ 2/main.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 
-
 int main() {
+    int n1;
+    cin >> n1;
+    vector<signed char> s1 = vector<signed char>(n1);
+    vector<int> a1 = vector<int>(n1);
+    for (int i = 0; i < n1; ++i) {
+        cin >> s1[i];
+    }
+    for (int i = 0; i < n1; ++i) {
+        cin >> a1[i];
+    }
+    int n2;
+    cin >> n2;
+    vector<signed char> s2 = vector<signed char>(n2);
+    vector<int> a2 = vector<int>(n2);
+    for (int i = 0; i < n2; ++i) {
+        cin >> s2[i];
+    }
+    for (int i = 0; i < n2; ++i) {
+        cin >> a2[i];
+    }
+    for (int i = 0; i < n1; ++i) {
+
+    }
+
+
 
     return 0;
 }
